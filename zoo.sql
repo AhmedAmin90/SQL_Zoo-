1e75df75f791fa55f96d6e20835910d28c609bd4
Ahmed Amin <qa.ahmed.amin10@gmail.com>
2021-01-08 00:27:22 +0300

    sixth quiz

--- zoo.sql
+++ zoo.sql
@@ -262,3 +262,87 @@ SELECT continent FROM world
 GROUP BY continent
 HAVING SUM(population) >= 100000000
 
+-- Sixth:
+
+-- 1
+SELECT matchid , player 
+ FROM goal 
+  WHERE teamid = 'GER'
+
+-- 2
+SELECT id,stadium,team1,team2
+  FROM game
+WHERE id = 1012
+
+-- 3
+SELECT  player, teamid, stadium , mdate
+  FROM game
+ JOIN goal
+ ON (game.id=goal.matchid)
+WHERE goal.teamid= 'GER'
+
+-- 4
+SELECT team1 , team2 , player
+FROM game
+JOIN goal
+ON (game.id=goal.matchid)
+WHERE goal.player LIKE 'Mario%'
+
+-- 5
+SELECT player, teamid, coach, gtime
+  FROM goal 
+JOIN eteam
+ON goal.teamid = eteam.id
+ WHERE gtime<=10
+
+--  6
+SELECT mdate , teamname
+FROM game JOIN eteam ON (team1 = eteam.id)
+WHERE eteam.coach = 'Fernando Santos'
+
+-- 7
+SELECT player
+FROM goal
+JOIN game
+ON goal.matchid = game.id 
+WHERE stadium  = 'National Stadium, Warsaw'
+
+-- 8
+ SELECT DISTINCT player
+ FROM game 
+ JOIN goal
+ ON matchid = id 
+ WHERE ((team1='GER' or team2='GER') and teamid != 'GER') 
+
+--  9
+
+SELECT teamname, count(player)
+FROM eteam JOIN goal ON id=teamid
+group BY teamname
+
+-- 10
+SELECT stadium , COUNT(player)
+FROM game
+JOIN goal
+ON id = goal.matchid
+GROUP BY stadium
+
+-- 11
+SELECT matchid,mdate, COUNT(teamid)
+  FROM game JOIN goal ON matchid = id 
+ WHERE (team1 = 'POL' OR team2 = 'POL')
+GROUP BY matchid ,mdate
+
+-- 12
+SELECT matchid,mdate, count(teamid)
+  FROM game JOIN goal ON matchid = id 
+ WHERE ( teamid = 'GER') GROUP BY matchid, mdate
+
+--  13
+SELECT mdate,
+  team1,
+  SUM(CASE WHEN teamid=team1 THEN 1 ELSE 0 END) score1,
+  team2,
+  SUM(CASE WHEN teamid=team2 THEN 1 ELSE 0 END) score2
+  FROM game LEFT JOIN goal ON matchid = id 
+GROUP BY mdate,matchid,team1,team2
