aa34c6537fa2ac9d4139e8ecdf99a9fa431ec33e
Ahmed Amin <qa.ahmed.amin10@gmail.com>
2021-01-07 20:24:27 +0300

    Fourth quiz

--- zoo.sql
+++ zoo.sql
@@ -154,3 +154,73 @@ SELECT winner, subject
   FROM nobel
  WHERE yr=1984
  ORDER BY subject IN ('Physics','Chemistry'), subject,winner
+ 
+ 
+ -- Fourth:
+
+-- 1
+SELECT name FROM world
+  WHERE population >
+     (SELECT population FROM world
+      WHERE name='Russia')
+
+-- 2
+SELECT name FROM world
+WHERE continent = 'Europe' AND gdp/population > (SELECT gdp/population FROM world WHERE name = 'United Kingdom' )
+
+-- 3
+SELECT name, continent 
+FROM world
+WHERE continent IN ( 
+SELECT continent
+FROM world 
+WHERE name = 'Argentina' OR name = 'Australia') ORDER BY name
+
+-- 4
+
+SELECT name , population
+FROM world
+WHERE population> (
+SELECT population FROM world
+WHERE name = 'Canada') AND population < (
+SELECT population FROM world
+WHERE name = 'Poland')
+
+-- 5
+SELECT name , CONCAT(ROUND((population/(
+SELECT population FROM world WHERE name = 'Germany'
+)) * 100,0),'%')
+FROM world
+WHERE continent = 'Europe' 
+
+-- 6
+SELECT name FROM world
+WHERE gdp > ( SELECT MAX(gdp) FROM world
+WHERE continent = 'Europe' )
+
+
+-- 7
+  SELECT continent, name, area FROM world x
+    WHERE area >= ALL
+      (SELECT area FROM world y
+          WHERE y.continent=x.continent
+            AND area>0)
+
+
+-- 8
+  SELECT continent, name FROM world x
+    WHERE name <= ALL
+      (SELECT name FROM world y
+          WHERE x.continent=y.continent)
+
+-- 9
+SELECT name , continent , population FROM world x
+WHERE population <= ALL ( SELECT name FROM world y
+WHERE x.continent = y.continent AND population >=
+25000000)
+
+-- 10
+      SELECT name, continent FROM world x
+            WHERE population / 3  > ALL
+             (SELECT population FROM world y
+                  WHERE x.continent=y.continent and population > 0 and y.name NOT LIKE x.name)
