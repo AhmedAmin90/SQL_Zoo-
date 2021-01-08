588115318aa26a85e1834123210a102c67501f91
Ahmed Amin <qa.ahmed.amin10@gmail.com>
2021-01-08 23:16:42 +0300

    Eighth quiz

--- zoo.sql
+++ zoo.sql
@@ -467,3 +467,64 @@ WHERE movieid IN
        FROM actor 
         WHERE name = 'Art Garfunkel')))
        AND name != 'Art Garfunkel'
+
+       
+-- Eighth:
+-- 1
+SELECT name 
+FROM teacher
+WHERE dept IS NULL
+
+-- 2
+SELECT teacher.name, dept.name
+ FROM teacher INNER JOIN dept
+           ON (teacher.dept=dept.id)
+
+-- 3
+SELECT teacher.name , dept.name FROM teacher
+LEFT JOIN dept
+ON teacher.dept = dept.id
+
+-- 4
+SELECT teacher.name , dept.name FROM teacher
+RIGHT JOIN dept
+ON teacher.dept = dept.id
+
+-- 5
+SELECT teacher.name , COALESCE(mobile,'07986 444 2266')
+FROM teacher
+
+-- 6
+SELECT teacher.name, COALESCE(dept.name , 'None')
+FROM teacher
+LEFT JOIN dept
+ON teacher.dept = dept.id
+
+-- 7
+SELECT COUNT(teacher.name) , COUNT(mobile)
+FROM teacher
+
+
+-- 8
+SELECT dept.name , COUNT(teacher.name)
+FROM  teacher
+RIGHT JOIN dept
+ON dept.id = teacher.dept
+GROUP BY dept.name
+
+-- 9
+SELECT teacher.name , 
+CASE
+WHEN dept BETWEEN 1 AND 2 THEN 'Sci'
+ELSE 'Art'
+END
+FROM teacher
+
+-- 10
+SELECT teacher.name , 
+CASE
+WHEN dept BETWEEN 1 AND 2 THEN 'Sci'
+WHEN dept = 3 THEN 'Art'
+ELSE 'None'
+END
+FROM teacher
